69d3ff9dcccc667790e4b2c9eba65b0258dfb3ae
Mriduljoly <117192996+Mriduljoly@users.noreply.github.com>
2023-02-09 23:00:58 +0530

    Update EmpDboard.jsx

--- src/Components/EmpDboard.jsx
+++ src/Components/EmpDboard.jsx
@@ -1,6 +1,6 @@
 import {React, useState,useEffect} from 'react'
-import axios from "axios"
-import Table from "react-bootstrap/Table"
+import axios from "axios";
+import Table from "react-bootstrap/Table";
 import "./Components/Eform.css";
 const EmpDboard = () => {
  
